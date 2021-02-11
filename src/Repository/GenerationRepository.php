b772095c261db6935be5b693b671ef5be53ad90a
roman <a100000a@yandex.ru>
2021-02-11 18:25:14 +0300

    upd symfony to 4.4.19

--- src/Repository/GenerationRepository.php
+++ src/Repository/GenerationRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Generation;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Generation|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class GenerationRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Generation::class);
     }
--- src/Repository/MarkaRepository.php
+++ src/Repository/MarkaRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Marka;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Marka|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class MarkaRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Marka::class);
     }
